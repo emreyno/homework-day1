c6708ffc0a6cf0fbff8b02b8e3a79a60d88cd9e6
Justin <earon.pabon@gmail.com>
2020-06-23 17:27:51 +0800

    Blank task manager

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Tasks from './components/Tasks/Tasks';
-import TaskFilter from './components/TaskFilter/TaskFilter';
-import AddTask from './components/AddTask/AddTask';
 
 class App extends Component {
-  state = {
-    filter: 'all' 
-  }
 
-  changeSelectHandler = (event) =>{
-    this.setState({ filter: event.target.value })
-  }
-
-  render(){
+  render() {
     return (
       <div className="App">
-        <TaskFilter changeCategory={this.changeSelectHandler}/>
-        <AddTask/>
-        <Tasks filter={this.state.filter} />
+        <h1>Task Manager</h1>
       </div>
     );
   }
