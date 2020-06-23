13a209480ac98d8a63a416d357b2a226c7658e2f
Justin <earon.pabon@gmail.com>
2020-06-23 17:53:30 +0800

    Static state part

--- src/App.js
+++ src/App.js
@@ -2,14 +2,39 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    tasks: [
+      {
+        title: 'Clean floor',
+        completed: 'false',
+      },
+      {
+        title: 'Give the Dog a Bath',
+        completed: 'false',
+      },
+      {
+        title: 'Order Takeout food',
+        completed: 'true',
+      },
+    ],
+  };
 
   render() {
     return (
       <div className="App">
         <h1>Task Manager</h1>
-        <div className="Task">Clean Bathroom</div>
-        <div className="Task">Feed Dog</div>
-        <div className="Task">Order Takeout food</div>
+        <div className="Task">
+          <h3>{this.state.tasks[0].title}</h3>
+          <p>Done: {this.state.tasks[0].completed}</p>
+        </div>
+        <div className="Task">
+          <h3>{this.state.tasks[1].title}</h3>
+          <p>Done: {this.state.tasks[1].completed}</p>
+        </div>
+        <div className="Task">
+          <h3>{this.state.tasks[2].title}</h3>
+          <p>Done: {this.state.tasks[2].completed}</p>
+        </div>
       </div>
     );
   }
